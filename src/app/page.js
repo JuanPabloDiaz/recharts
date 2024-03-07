7492f1041531211e2522178a61ab9810e1a0d470
JuanPabloDiaz <juanchodis@hotmail.com>
2024-03-07 11:47:31 -0500

    Add PieChart component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
-import AreaChart from './components/AreaChart';
-import BarChart from './components/BarChart';
-import LineChart from './components/LineChart';
+import AreaChart from "./components/AreaChart";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-4 md:px-8 xl:px-10 py-44">
       <div className="grid xl:grid-cols-3 lg:grid-cols-2 w-full gap-10 max-w-[1400px]">
+        <GridItem title="Pie Chart">
+          <PieChart />
+        </GridItem>
+
         <GridItem title="Area Chart">
           <AreaChart />
         </GridItem>
