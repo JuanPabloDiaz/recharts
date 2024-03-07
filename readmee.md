4e8af91be64496916fe53d4334bc5571a209926f
JuanPabloDiaz <juanchodis@hotmail.com>
2024-03-07 14:14:10 -0500

    Add RadarChart component and update dependencies in package.json

---
+++ readmee.md
@@ -0,0 +1,19 @@
+# Recharts Library
+
+Reacharts is a composable charting library for building charts with ease. It is built on top of D3. This library is a wrapper around D3 and it is used to create beautiful charts in React.
+
+Learn more about [Recharts](http://recharts.org/en-US/).
+
+[![](https://img.youtube.com/vi/Fu_YFp-9xoQ/0.jpg)]()
+
+## Installation
+
+```bash
+npm install recharts
+```
+
+### Resources:
+
+- [Recharts Documentation](http://recharts.org/en-US/)
+- [GitHub recharts-library](https://github.com/CodeSnaps/recharts-library)
+- [YouTube Video 📺](https://www.youtube.com/watch?v=Fu_YFp-9xoQ) Create Beautiful Charts in React With Recharts in 7 Minutes
--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,16 @@ import BarChart from "./components/BarChart";
 import LineChart from "./components/LineChart";
 import PieChart from "./components/PieChart";
 import ComposedChart from "./components/ComposedChart";
+import RadarChart from "./components/RadarChart";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-4 md:px-8 xl:px-10 py-44">
       <div className="grid xl:grid-cols-3 lg:grid-cols-2 w-full gap-10 max-w-[1400px]">
+        <GridItem title="Radar Chart">
+          <RadarChart />
+        </GridItem>
+
         <GridItem title="Composed Chart">
           <ComposedChart />
         </GridItem>
