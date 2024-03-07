a0ad4e93426fbc725d8addb378662bce0c53dbfe
JuanPabloDiaz <juanchodis@hotmail.com>
2024-03-07 14:34:05 -0500

    Add Treemap and rearrange chart order

--- src/app/page.js
+++ src/app/page.js
@@ -6,41 +6,38 @@ import ComposedChart from "./components/ComposedChart";
 import RadarChart from "./components/RadarChart";
 import RadialBarChart from "./components/RadialBarChart";
 import FunnelChart from "./components/FunnelChart";
+import Treemap from "./components/Treemap";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-4 md:px-8 xl:px-10 py-44">
       <div className="grid xl:grid-cols-3 lg:grid-cols-2 w-full gap-10 max-w-[1400px]">
-        <GridItem title="Funnel Chart">
-          <FunnelChart />
-        </GridItem>
-
-        <GridItem title="Radial Bar Chart">
-          <RadialBarChart />
-        </GridItem>
-
-        <GridItem title="Radar Chart">
-          <RadarChart />
-        </GridItem>
-
         <GridItem title="Composed Chart">
           <ComposedChart />
         </GridItem>
-
-        <GridItem title="Pie Chart">
-          <PieChart />
+        <GridItem title="Radar Chart">
+          <RadarChart />
         </GridItem>
-
         <GridItem title="Area Chart">
           <AreaChart />
         </GridItem>
-
+        <GridItem title="Pie Chart">
+          <PieChart />
+        </GridItem>
         <GridItem title="Bar Chart">
           <BarChart />
         </GridItem>
-
+        <GridItem title="Funnel Chart">
+          <FunnelChart />
+        </GridItem>
         <GridItem title="Line Chart">
           <LineChart />
+        </GridItem>
+        <GridItem title="Treemap">
+          <Treemap />
+        </GridItem>
+        <GridItem title="Radial Bar Chart">
+          <RadialBarChart />
         </GridItem>
       </div>
     </main>
