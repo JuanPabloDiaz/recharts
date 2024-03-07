14288201bbcae41f99d72353983cc3fb6d7daba0
JuanPabloDiaz <juanchodis@hotmail.com>
2024-03-07 14:20:07 -0500

    Add Radial Bar Chart component

---
+++ src/app/components/RadialBarChart.jsx
@@ -0,0 +1,89 @@
+"use client";
+
+import {
+  Legend,
+  RadialBar,
+  RadialBarChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+
+const data = [
+  {
+    name: "18-24",
+    uv: 31.47,
+    pv: 2400,
+    fill: "#8884d8",
+  },
+  {
+    name: "25-29",
+    uv: 26.69,
+    pv: 4567,
+    fill: "#83a6ed",
+  },
+  {
+    name: "30-34",
+    uv: -15.69,
+    pv: 1398,
+    fill: "#8dd1e1",
+  },
+  {
+    name: "35-39",
+    uv: 8.22,
+    pv: 9800,
+    fill: "#82ca9d",
+  },
+  {
+    name: "40-49",
+    uv: -8.63,
+    pv: 3908,
+    fill: "#a4de6c",
+  },
+  {
+    name: "50+",
+    uv: -2.63,
+    pv: 4800,
+    fill: "#d0ed57",
+  },
+  {
+    name: "unknow",
+    uv: 6.67,
+    pv: 4800,
+    fill: "#ffc658",
+  },
+];
+
+const RadialBarChartComponent = () => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <RadialBarChart
+        width={730}
+        height={250}
+        innerRadius="10%"
+        outerRadius="80%"
+        data={data}
+        startAngle={180}
+        endAngle={0}
+      >
+        <RadialBar
+          minAngle={15}
+          label={{ fill: "#666", position: "insideStart" }}
+          background
+          clockWise={true}
+          dataKey="uv"
+        />
+        <Legend
+          iconSize={10}
+          width={120}
+          height={140}
+          layout="vertical"
+          verticalAlign="middle"
+          align="right"
+        />
+        <Tooltip />
+      </RadialBarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default RadialBarChartComponent;
--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,16 @@ import LineChart from "./components/LineChart";
 import PieChart from "./components/PieChart";
 import ComposedChart from "./components/ComposedChart";
 import RadarChart from "./components/RadarChart";
+import RadialBarChart from "./components/RadialBarChart";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-4 md:px-8 xl:px-10 py-44">
       <div className="grid xl:grid-cols-3 lg:grid-cols-2 w-full gap-10 max-w-[1400px]">
+        <GridItem title="Radial Bar Chart">
+          <RadialBarChart />
+        </GridItem>
+
         <GridItem title="Radar Chart">
           <RadarChart />
         </GridItem>
