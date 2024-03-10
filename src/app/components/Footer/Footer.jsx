afc6a3aa64c037e3049be8c8bf4bd491f2a59640
JuanPabloDiaz <juanchodis@hotmail.com>
2024-03-10 05:17:52 -0400

    Add icons to Footer component

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 
+// Icons
 export function Github(props) {
   // MingcuteGithubLine
   return (
@@ -57,6 +58,26 @@ export function Twitter(props) {
   );
 }
 
+export function Api(props) {
+  // GravityUiAbbrApi
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 16 16"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M5.02 11.419L4.691 10H2.808l-.327 1.419a.75.75 0 1 1-1.462-.338L2.407 5.07a1.379 1.379 0 0 1 2.686 0l1.388 6.012a.75.75 0 1 1-1.462.338Zm-1.27-5.5L4.346 8.5H3.154zM14.5 4.75a.75.75 0 1 0-1.5 0v6.5a.75.75 0 0 0 1.5 0zM8.25 4a.75.75 0 0 0-.75.75v6.5a.75.75 0 0 0 1.5 0V9.5h.5a2.75 2.75 0 0 0 0-5.5zM9.5 8H9V5.5h.5a1.25 1.25 0 0 1 0 2.5"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
+
 export const Footer = () => {
   const links = [
     { href: "api/area", text: "Area" },
@@ -88,7 +109,10 @@ export const Footer = () => {
   return (
     <div className="mt-2 py-4 w-full">
       <div className="text-center pb-10">
-        <h3 className="text-white/40 pb-5">APIs</h3>
+        <div className="text-white/40 pb-5 flex justify-center">
+          <Api className="w-8 h-8" />
+        </div>
+
         <p className="text-yellow-200/60 p-4">
           Data sources I used to create the charts
         </p>
